cde8f292d098255263332244515413ca21cb623c
Fabian Huber <77811859+fabs0@users.noreply.github.com>
2021-06-08 08:30:14 +0200

    Update blackjack.py

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -1,1 +1,15 @@
-def awd:
+# blackjack py
+
+standard_deck =[
+  "CA","C2","C3","C4","C5","C6","C7","C8","C9","C10","CJack","CQueen","CKing",
+  "DA","D2","D3","D4","D5","D6","D7","D8","D9","D10","DJack","DQueen","DKing",
+  "HA","H2","H3","H4","H5","H6","H7","H8","H9","H10","HJack","HQueen","HKing",
+  "SA","S2","S3","S4","S5","S6","S7","S8","S9","S10","SJack","SQueen","SKing"
+               ]
+
+def player_hit():
+  
+def player_stand():
+  
+def player_double():
+  
